02b2c5805a3c6c4675dc5d7c8a3755a84c90a8c0
Vishan Budhathoki <vishanbudhathoki123@gmail.com>
2025-02-25 14:56:32 +0545

    Caching queries and Invalidating Cache

--- book-review/app/Http/Controllers/BookController.php
+++ book-review/app/Http/Controllers/BookController.php
@@ -26,10 +26,13 @@ class BookController extends Controller
             'highest_rated_last_month' => $books->highestRatedLastMonth(),
             'highest_rated_last_6months' => $books->highestRatedLast6Months(),
 
-            default => $books->Latest()
+            default => $books->latest()
         };
 
-        $books = $books->get();
+        // $books = $books->get();
+
+        $cacheKey = 'books:' . $filter . ':' . $title;
+        $books = cache()->remember($cacheKey, 3600, fn() => $books->get());
 
         return view('books.index', ['books' => $books]);
     }
@@ -53,9 +56,15 @@ class BookController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(Book $book)
     {
-        //
+        $cacheKey = 'book:' . $book->id;
+
+        $book =cache()->remember($cacheKey,3600,fn() => $book->load([
+                'reviews' => fn($query) => $query->latest()
+        ]));
+
+        return view('books.show',['book'=>$book]);
     }
 
     /**
